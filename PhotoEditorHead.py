154e37f39bb3673c798a834bac4e9bf459ac253e
AlexHopeIT <dexter4445@gmail.com>
2022-08-30 14:50:48 +0300

    Add selection function

--- PhotoEditorHead.py
+++ PhotoEditorHead.py
@@ -12,6 +12,15 @@ class PhotoEditor:
         # self.root.configure(height=150, width=150, bg='#943232') ?????????? how????
         self.image_tabs = Notebook(self.root)
         self.opened_images = []
+
+        self.selection_top_x = 0
+        self.selection_top_y = 0
+        self.selection_bottom_x = 0
+        self.selection_bottom_y = 0
+
+        self.canvas_for_selection = None
+        self.selection_rect = None
+
         self.init()
 
     def init(self):
@@ -20,6 +29,7 @@ class PhotoEditor:
 
         self.image_tabs.enable_traversal()
         self.root.bind('<Escape>', self._close)
+        self.root.protocol('WM_DELETE_WINDOW', self._close)
 
     def run(self):
         self.draw_menu()
@@ -35,10 +45,9 @@ class PhotoEditor:
         menu_file.add_separator()
         menu_file.add_command(label='Save as', command=self.save_image_as)
         menu_file.add_command(label='Save', command=self.save_image_in_program)
+        menu_file.add_command(label='Save all', command=self.save_all_images)
         menu_file.add_separator()
         menu_file.add_command(label='Exit', command=self._close)
-
-
 
         self.root.configure(menu=string_menu)
 
@@ -50,7 +59,6 @@ class PhotoEditor:
         rotate_menu.add_command(label='Rotate right by 90', command=lambda: self.rotate_current_image(-90))
         rotate_menu.add_command(label='Rotate left by 180', command=lambda: self.rotate_current_image(180))
         rotate_menu.add_command(label='Rotate right by 180', command=lambda: self.rotate_current_image(-180))
-
 
         flip_menu = Menu(edit_menu, tearoff=0)
         flip_menu.add_command(label='Flip horizontally', command=lambda: self.flip_image('horizontally'))
@@ -80,6 +88,9 @@ class PhotoEditor:
         filter_menu.add_command(label='Smooth', command=lambda: self.impose_filter(ImageFilter.SMOOTH))
         filter_menu.add_command(label='Smooth more', command=lambda: self.impose_filter(ImageFilter.SMOOTH_MORE))
 
+        crop_menu = Menu(edit_menu, tearoff=0)
+        crop_menu.add_command(label='Start selection', command=self.start_selection)
+        crop_menu.add_command(label='Stop selection', command=self.stop_selection)
 
         string_menu.add_cascade(label='File', menu=menu_file)
         string_menu.add_cascade(label='Edit', menu=edit_menu)
@@ -88,6 +99,7 @@ class PhotoEditor:
         edit_menu.add_cascade(label='Flip', menu=flip_menu)
         edit_menu.add_cascade(label='Resize', menu=resize_menu)
         edit_menu.add_cascade(label='Filters', menu=filter_menu)
+        edit_menu.add_cascade(label='Crop', menu=crop_menu)
 
     def draw_widgets(self):
         self.image_tabs.pack(fill='both', expand=1)
@@ -104,9 +116,10 @@ class PhotoEditor:
 
         image_tab = Frame(self.image_tabs)
 
-        image_label = Label(image_tab, image=image_tk)
-        image_label.image = image_tk
-        image_label.pack(side='bottom', fill='both', expand=True)
+        image_panel = Canvas(image_tab, width=image_tk.width(), height=image_tk.height(), bd=0, highlightthickness=0)
+        image_panel.image = image_tk
+        image_panel.create_image(0, 0, image=image_tk, anchor='nw')
+        image_panel.pack(expand='yes')
 
         self.image_tabs.add(image_tab, text=paths_to_images.split('/')[-1])
         self.image_tabs.select(image_tab)
@@ -163,15 +176,26 @@ class PhotoEditor:
 
         self.add_new_image(path_for_save + new_ext)
 
+    def save_all_images(self):
+        for index, (path, image) in enumerate(self.opened_images):
+            if path[-1] != '*':
+                continue
+            path = path[:-1]
+            self.opened_images[index][0] = path
+            image.save(path)
+            self.image_tabs.tab(index, text=path.split('/')[-1])
 
     def update_image_in_app(self, current_tab, image):
         tab_number = self.image_tabs.index(current_tab)
         tab_frame = self.image_tabs.children[current_tab[current_tab.rfind('!'):]]
-        label = tab_frame.children['!label']
+        canvas = tab_frame.children['!canvas']
         self.opened_images[tab_number][1] = image
         image_tk = ImageTk.PhotoImage(image)
-        label.configure(image=image_tk)
-        label.image = image_tk
+
+        canvas.delete('all')
+        canvas.image = image_tk
+        canvas.configure(width=image_tk.width(), height=image_tk.height())
+        canvas.create_image(0, 0, image=image_tk, anchor='nw')
 
         image_path = self.opened_images[tab_number][0]
         if image_path[-1] != '*':
@@ -221,7 +245,55 @@ class PhotoEditor:
         image = image.filter(filter_type)
         self.update_image_in_app(current_tab, image)
 
+    def start_selection(self):
+        current_tab = self.image_tabs.select()
+        if not current_tab:
+            return
+        tab_frame = self.image_tabs.children[current_tab[current_tab.rfind('!'):]]
+        canvas = tab_frame.children['!canvas']
+
+        self.canvas_for_selection = canvas
+        self.selection_rect = canvas.create_rectangle(self.selection_top_x, self.selection_top_y,
+                                                      self.selection_bottom_x, self.selection_bottom_y,
+                                                      dash=(10, 10), fill='', outline='yellow', width=3
+                                                      )
+
+        canvas.bind("<Button-1>", self.get_selection_start_pos)
+        canvas.bind("<B1-Motion>", self.update_selection_end_pos)
+
+    def get_selection_start_pos(self, event):
+        self.selection_top_x, self.selection_top_y = event.x, event.y
+
+    def update_selection_end_pos(self, event):
+        self.selection_bottom_x, self.selection_bottom_y = event.x, event.y
+        if self.canvas_for_selection is not None and self.selection_rect is not None:
+            self.canvas_for_selection.coords(
+                self.selection_rect,
+                self.selection_top_x, self.selection_top_y,
+                self.selection_bottom_x, self.selection_bottom_y
+            )
+
+    def stop_selection(self):
+        self.canvas_for_selection.unbind("<Button-1>")
+        self.canvas_for_selection.unbind("<B1-Motion>")
+
+        self.canvas_for_selection.delete(self.selection_rect)
+
+        self.selection_rect = None
+        self.canvas_for_selection = None
+        self.selection_top_x, self.selection_top_y = 0, 0
+        self.selection_bottom_x, self.selection_bottom_y = 0, 0
+
+    def unsaved_images(self):
+        for path, _ in self.opened_images:  # "_" is "image"
+            if path[-1] == '*':
+                return True
+            return False
+
     def _close(self, event=None):
+        if self.unsaved_images():
+            if not mb.askyesno('There are unsaved changes of images', 'You need to save changes. Exit anyway?'):
+                return
         self.root.quit()
 
 
